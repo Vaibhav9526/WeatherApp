d13af28871145fd79f1ca8903d0479e8a9369fc9
vaibhav9526 <vaibhavsharma9526@gmail.com>
2024-11-28 16:41:34 +0530

    updated ui , shorter js and added new preloader

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// API KEY ="40912be1499980a4e22d60c80e398463"
-// Animation complete event listener
-document
-  .getElementById("lottieAnimation")
-  .addEventListener("complete", function () {
-    document.querySelector(".loader").style.display = "none";
-    document.querySelector(".content").style.display = "block";
-  });
-
-// api work below
+const PreLoader = document.querySelector(".loader");
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    PreLoader.style.display = "none";
+  }, 3500);
+});
 
 const apikey = "40912be1499980a4e22d60c80e398463";
 const apiurl =
@@ -20,54 +16,26 @@ const weathericon = document.querySelector(".weather-icon");
 async function checkweather(city) {
   const response = await fetch(apiurl + city + `&appid=${apikey}`);
 
-  if (response.status == 400) {
-    alert("PLEASE ENTER CITY NAME");
+  if (response.status == 400 || response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    alert("INVALID CITY NAME");
   } else {
-    if (response.status == 404) {
-      document.querySelector(".error").style.display = "block";
-      alert("INVALID CITY NAME");
-      console.log("invalid city name");
-    } else {
-      var data = await response.json();
-      document.querySelector(".weathername").innerHTML =
-        data.weather[0].main + " (" + data.weather[0].description + ")";
+    var data = await response.json();
 
-      if (data.sys.country == null) {
-        document.querySelector(".city").innerHTML = data.name;
-      } else {
-        document.querySelector(".city").innerHTML =
-          data.name + "  (" + data.sys.country + ")";
-      }
-
-      document.querySelector(".temp").innerHTML =
-        Math.round(data.main.temp) + "°C";
+      document.querySelector(".weathername").innerHTML = data.weather[0].main + " (" + data.weather[0].description + ")";
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
       document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
       document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
-      document.querySelector(".owm").href =
-        "https://openweathermap.org/city/" + data.id;
+      document.querySelector(".owm").href = "https://openweathermap.org/city/" + data.id;
 
-      if (data.weather[0].main == "Clouds") {
-        weathericon.src = "assests/clouds.png";
-      } else if (data.weather[0].main == "Clear") {
-        weathericon.src = "assests/clear.png";
-      } else if (data.weather[0].main == "Rain") {
-        weathericon.src = "assests/rain.png";
-      } else if (data.weather[0].main == "Mist") {
-        weathericon.src = "assests/mist.png";
-      } else if (data.weather[0].main == "Drizzle") {
-        weathericon.src = "assests/drizzle.png";
-      } else if (data.weather[0].main == "Snow") {
-        weathericon.src = "assests/snow.png";
-      } else if (data.weather[0].main == "Haze") {
-        weathericon.src = "assests/haze.png";
-      }
+    var WeatherType = data.weather[0].main;
+    weathericon.src = `assests/${WeatherType}.png`;
+
       document.querySelector(".weather").style.display = "block";
       document.querySelector(".error").style.display = "none";
       document.querySelector(".blink").style.display = "none";
-
-      console.log('JS BY VAIBHAV SHARMA');
-      console.log(data);
-    }
+      // console.log(data);
   }
 }
 
@@ -75,9 +43,8 @@ searchbtn.addEventListener("click", () => {
   checkweather(searchbox.value);
 });
 
-
-searchbox.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
+searchbox.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
     checkweather(searchbox.value);
   }
 });
