23da8b2f8ffa8bf7921642b4957ca4946c1ab36a
vaibhav9526 <vaibhavsharma9526@gmail.com>
2024-12-11 17:29:04 +0530

    merge sunset and sunrise page into this, added few more column

--- weather.js
+++ weather.js
@@ -1,52 +1,58 @@
 const PreLoader = document.querySelector(".loader");//preloader
 const apikey = "40912be1499980a4e22d60c80e398463";//openweathermap api key
 const apiurl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q="; //weather api
+const ICONapi = "https://openweathermap.org/img/wn/"; //ICONS API
 const RevGeoAPI = "https://ipapi.co/json/"; //reverse geolocation api
 const searchbox = document.querySelector(".search input");//search input
 const searchbtn = document.querySelector(".search button");//search button
+const loadingSpinner = document.querySelector(".SpinLoader");//loading spinner
 const weathericon = document.querySelector(".weather-icon");//weather icon
 
 //function to check the weather
 async function checkweather(city) {
-    //Fetching weather data
-    const response = await fetch(apiurl + city + `&appid=${apikey}`);  
-    searchbox.value = city;
-    if (response.status == '' || response.status == 404 || response.status == 400) {
-    document.querySelector(".error").style.display = "block";
-    alert("INVALID CITY NAME");
-  } else {
-    var data = await response.json();
-    //displaying the weather data
-    document.querySelector(".weathername").innerHTML = data.weather[0].main + " (" + data.weather[0].description + ")";
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
-    document.querySelector(".owm").href = "https://openweathermap.org/city/" + data.id;
-    //displaying the weather icon
+    loadingSpinner.style.display = "block"; //loading spinner on city reloads
+    const response = await fetch(apiurl + city + `&appid=${apikey}`); //Fetching weather data
+    searchbox.value = city; //search input value for default city (IP address city)
+    if (response.status == '' || response.status == 404 || response.status == 400) { //error handling
+      document.querySelector(".error").style.display = "block";
+      alert("INVALID CITY NAME");
+    } else {
+    var data = await response.json();//fetching data
+    console.log(data)
     
-    const WeatherType = data.weather[0].main.toLowerCase();
-    weathericon.src = `/assests/${WeatherType}.png`;
+    document.querySelector(".weathername").innerHTML = data.weather[0].main + " (" + data.weather[0].description + ")"; //weather name
+    document.querySelector(".city").innerHTML = data.name; //city name
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"; //temperature
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"; //humidity
+    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h"; //wind speed
+    document.querySelector(".max-temp").innerHTML = Math.round(data.main.temp_max) + "°C"; //max temperature
+    document.querySelector(".min-temp").innerHTML = Math.round(data.main.temp_min) + "°C"; //min temperature
+    document.querySelector(".sunrise").innerHTML = new Date(data.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });//sunrise time
+    document.querySelector(".sunset").innerHTML = new Date(data.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });//sunset time
+    
+    weathericon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png `; //weather icon from openweathermap
+    
     //displaying the card
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
+    loadingSpinner.style.display = "none";
     }
 }
-window.addEventListener("load", async () => {
+
+window.addEventListener("load", async () => {//onload preloader and Default location
   setTimeout(() => {
     PreLoader.style.display = "none";
-  }, 3500);
-  //(it will use the ip address to get the city name by Reverse geolocating)
-  const responseREV = await fetch(RevGeoAPI);
+  }, 3000);
+  const responseREV = await fetch(RevGeoAPI);//(it will use the ip address to get the city name by Reverse geolocating)
   const dataREV = await responseREV.json();
   var defCity = dataREV.city;
   checkweather(defCity);
 });
-searchbtn.addEventListener("click", () => {
+searchbtn.addEventListener("click", () => { //search button click event
   checkweather(searchbox.value);
 });
 
-searchbox.addEventListener("keypress", (e) => {
+searchbox.addEventListener("keypress", (e) => { //search input keypress event
   if (e.key === "Enter") {
     checkweather(searchbox.value);
   }
